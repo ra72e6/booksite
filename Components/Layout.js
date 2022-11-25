49ff224e6b6c81646f6d3fcd41e21759b0c4eb77
ra72e6 <ra72e6@hanmail.net>
2022-11-25 15:41:21 +0900

    3th

--- Components/Layout.js
+++ Components/Layout.js
@@ -12,22 +12,22 @@ export default function Layout({ title, children }) {
       <div className="flex min-h-screen flex-col justify-between">
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md bg-slate-200">
-            <Link href="/">
-              <a className="text-lg font-bold">Next</a>
+            <Link href="/" className="text-lg font-bold">
+              Booksite
             </Link>
             <div>
-              <Link href="/cart">
-                <a className="p-2">Cart</a>
+              <Link href="/cart" className="p-2">
+                카트
               </Link>
-              <Link href="/login">
-                <a className="p-2">Login</a>
+              <Link href="/login" className="p-2">
+                로그인
               </Link>
             </div>
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
         <footer className="flex h-10 justify-center items-center shadow-inner bg-red-100">
-          <p>Copyright &copy; 2022 NextShop</p>
+          <p>양노임한 팀프로젝트</p>
         </footer>
       </div>
     </>
