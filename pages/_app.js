b627d08677403ead7ab685b331a4f42d4ce4a85a
ra72e6 <ra72e6@hanmail.net>
2022-11-30 10:17:58 +0900

    11th

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
 
